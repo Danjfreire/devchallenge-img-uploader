8451e204b6d1492038dddaaeee42f7e9ef05dfe3
Daniel Freire <danjfreire@gmail.com>
2021-01-18 20:54:49 -0300

    feat: added upload component initial layout and style

--- app/src/App.js
+++ app/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1 className="header">Upload your image</h1>
+
+      <p className="">File should be Jpeg, Png...</p>
+
+      <div className="uploader">
+        <div className="logo"></div>
+        <p>Drag & drop your image here</p>
+      </div>
+
+      <p className>Or</p>
+
+      <button>Choose a file</button>
     </div>
   );
 }
